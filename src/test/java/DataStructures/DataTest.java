c3536d704b548ef29d399d45b7a0e41a48df3eb1
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-10-10 13:13:40 +0530

    [swapnaneel]UC8:Ability to insert 40 after 30

--- src/test/java/DataStructures/DataTest.java
+++ src/test/java/DataStructures/DataTest.java
@@ -117,4 +117,20 @@ public class DataTest {
 		myLinkedList.add(mythirdNode);
 		INode res=myLinkedList.search(30);
 	}
+	@Test
+	public void abilityToInsert40After30()
+	{
+		Node<Integer> mythirdNode=new Node<>(56);
+		Node<Integer> mysecondNode=new Node<>(30);
+		Node<Integer> myfirstNode=new Node<>(70);
+		Node<Integer> myfourthNode=new Node<>(40);
+		myfirstNode.setNext(mysecondNode);
+		mysecondNode.setNext(mythirdNode);
+		LinkedList myLinkedList=new LinkedList();
+		myLinkedList.add(myfirstNode);
+		myLinkedList.add(mysecondNode);
+		myLinkedList.add(mythirdNode);
+		INode res=myLinkedList.search(30);
+		myLinkedList.insert(res, myfourthNode);
+	}
 }
