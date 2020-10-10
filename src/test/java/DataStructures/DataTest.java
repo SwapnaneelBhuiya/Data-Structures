8023030aad2ece308a4ac68587f9f7734f12860e
SwapnaneelBhuiya <43994683+SwapnaneelBhuiya@users.noreply.github.com>
2020-10-10 12:21:39 +0530

    [swapnaneel]UC2:Ability to create Linked List by adding 30 and 56 to 70

--- src/test/java/DataStructures/DataTest.java
+++ src/test/java/DataStructures/DataTest.java
@@ -4,20 +4,39 @@ import org.junit.Test;
 
 public class DataTest {
 	@Test
+	@Test
 	public void given3numbersWhenLinkedShouldPassedLinedListTest()
 	{
-		Node<Integer> mythirdNode=new Node<>(56);
+		Node<Integer> myfirstNode=new Node<>(56);
 		Node<Integer> mysecondNode=new Node<>(30);
-		Node<Integer> myfirstNode=new Node<>(70);
+		Node<Integer> mythirdNode=new Node<>(70);
 		myfirstNode.setNext(mysecondNode);
 		mysecondNode.setNext(mythirdNode);
-		boolean result=myfirstNode.getNext().equals(mysecondNode)
-				&&mysecondNode.getNext().equals(mythirdNode);
 		new LinkedList();
 		LinkedList myLinkedList=new LinkedList();
 		myLinkedList.add(myfirstNode);
 		myLinkedList.add(mysecondNode);
 		myLinkedList.add(mythirdNode);
 		myLinkedList.printMyNode();
+		boolean result=myfirstNode.getNext().equals(mysecondNode)
+				&&mysecondNode.getNext().equals(mythirdNode);
+		Assert.assertEquals(true,result);
+	}
+	@Test
+	public void given3numbersWhenAddedToLinkedListShouldBeAddedToTop()
+	{
+		Node<Integer> mythirdNode=new Node<>(56);
+		Node<Integer> mysecondNode=new Node<>(30);
+		Node<Integer> myfirstNode=new Node<>(70);
+		myfirstNode.setNext(mysecondNode);
+		mysecondNode.setNext(mythirdNode);
+		new LinkedList();
+		LinkedList myLinkedList=new LinkedList();
+		myLinkedList.add(myfirstNode);
+		myLinkedList.add(mysecondNode);
+		myLinkedList.add(mythirdNode);
+		boolean result=myfirstNode.getNext().equals(mysecondNode)
+				&&mysecondNode.getNext().equals(mythirdNode);
+		Assert.assertEquals(true,result);
 	}
 }
